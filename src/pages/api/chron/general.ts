71a8bfd4e472a5f06a75c7295b366e02d5a2e8d0
Mark Krasner <mzkrasner@gmail.com>
2024-05-16 14:14:24 -0400

    Combining referral patching and deform ingestion into one job

--- src/pages/api/chron/general.ts
+++ src/pages/api/chron/general.ts
@@ -5,6 +5,7 @@ import { calculate } from "@/utils/calculateScores";
 import { getAggregationCount } from "@/utils/readAggregationCount";
 import { getDeform } from "@/utils/getDeformData";
 import { calculateReferrals } from "@/utils/referrals";
+import { processReferralPoints } from "@/utils/processReferralPoints";
 
 interface Response extends NextApiResponse {
   status(code: number): Response;
@@ -13,7 +14,6 @@ interface Response extends NextApiResponse {
 
 export default async function handler(req: NextApiRequest, res: Response) {
   try {
-
     // fetch the DeForm data
     const rows = await getDeform().then((data) => {
       return data?.data;
@@ -21,9 +21,7 @@ export default async function handler(req: NextApiRequest, res: Response) {
 
     // failure mode for fetching DeForm data
     if (!rows) {
-      return res
-        .status(500)
-        .send({ error: "Unable to fetch DeForm data" });
+      return res.status(500).send({ error: "Unable to fetch DeForm data" });
     }
     //determine the total number of entries
     const totalEntries = rows.length;
@@ -38,9 +36,26 @@ export default async function handler(req: NextApiRequest, res: Response) {
         .send({ error: "Unable to fetch aggregation data" });
     }
 
+    // first, handle patching of referral scores for existing entries - start by calculating the referral scores
+    const finalReferralScores = await calculateReferrals({
+      rows,
+      startRow: aggregations,
+      chron: true,
+    });
+
+    // failure mode for calculating referral scores for existing entries
+    if ("error" in finalReferralScores) {
+      return res.status(500).send({ error: finalReferralScores.error });
+    }
+
+    // process and write the patches to Ceramic
+    const patchedReferralResults =
+      await processReferralPoints(finalReferralScores);
+
+    // next, handle new entries
+
     // set the difference between the total entries and the number of entries recorded
     const difference = totalEntries - aggregations;
-    
 
     // if there are new entries, calculate the final scores
     if (rows && difference > 0) {
@@ -53,18 +68,24 @@ export default async function handler(req: NextApiRequest, res: Response) {
         return res.status(500).send({ error: finalScores.error });
       }
 
-      const referralScores = await calculateReferrals({ rows, startRow: aggregations });
+      const referralScores = await calculateReferrals({
+        rows,
+        startRow: aggregations,
+      });
 
       // failure mode for calculating referral scores
       if ("error" in referralScores) {
         return res.status(500).send({ error: referralScores.error });
       }
-      
-      // process and write the scores to Ceramic
-      const results = await processSingleContextPoints([...finalScores, ...referralScores]);
 
-      if (results) {
-        res.status(200).send(results);
+      // process and write the scores to Ceramic
+      const results = await processSingleContextPoints([
+        ...finalScores,
+        ...referralScores,
+      ]);
+
+      if (results && patchedReferralResults) {
+        res.status(200).send([...results, ...patchedReferralResults]);
       } else {
         res.status(500).send({ error: "Internal Server Error" });
       }
