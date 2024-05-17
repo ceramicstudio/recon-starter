89acbdf2accb9ba7d61d5cc6388ccc46a4c404dc
Mark Krasner <mzkrasner@gmail.com>
2024-05-17 17:56:35 -0400

    Finishing up save customer

--- src/utils/pg/saveCustomer.ts
+++ src/utils/pg/saveCustomer.ts
@@ -4,16 +4,16 @@ import * as pg from "pg";
 const STRING = process.env.DATABASE_URL;
 const { Client, Pool } = pg;
 
-const questions = [
-  "How would you like to participate in the ecosystem?",
-  "Are you technical?",
-  "On a scale of 1-10, how excited are you about what ADM?",
-  "What is your X username? - Twitter Username",
-  "What is your email address?",
-  "What is your wallet address? - Wallet Address",
-  "What is your Discord handle? - Discord Display Name",
-  "What is your Discord handle? - Discord User ID",
-];
+const questions: Record<string, string> = {
+  participant: "How would you like to participate in the ecosystem?",
+  technical: "Are you technical?",
+  excitement: "On a scale of 1-10, how excited are you about what ADM?",
+  xUsername: "What is your X username? - Twitter Username",
+  email: "What is your email address?",
+  address: "What is your wallet address? - Wallet Address",
+  discordHandle: "What is your Discord handle? - Discord Display Name",
+  discordId: "What is your Discord handle? - Discord User ID",
+};
 
 export const saveCustomers = async (input: ScoreInput) => {
   try {
@@ -37,16 +37,34 @@ export const saveCustomers = async (input: ScoreInput) => {
 
     const { rows, startRow } = input;
 
+    const saved = [];
     // first start at startRow index while iterating through entry
     for (let i = startRow; i < rows.length; i++) {
+      const userAnswers = {} as Record<string, string | string[] | undefined>;
       // iterate through questions and find the relevant answer object based on if the name matches the key
       const answers = rows[i]?.answers;
 
-     // need to finish
-      
+      for (const key in questions) {
+        // locate the correct answer as a string
+        const question = questions[key];
+        const answer = answers?.find((answer) => answer.name === question);
+        userAnswers[key] = answer?.value;
+      }
+      const saveCustomerQuery = `INSERT INTO customers (participant, technical, excitement, xUsername, email, address, discordHandle, discordId) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`;
+      const saveCustomer = await client.query(saveCustomerQuery, [
+        userAnswers.participant,
+        userAnswers.technical ? userAnswers.technical[0] === "Yes" : false,
+        Number(userAnswers.excitement),
+        userAnswers.xUsername,
+        userAnswers.email,
+        userAnswers.address,
+        userAnswers.discordHandle,
+        userAnswers.discordId,
+      ]);
+      saved.push(saveCustomer.rows[0]);
     }
     await client.end();
-    return;
+    return saved as Array<Record<string, string | number | string[] | boolean>>;
   } catch (error) {
     console.error(error);
     return { error: "Internal Server Error" };
